57689881def229e1084f2999e71d4414fdc67019
alsritter <alsritter@outlook.com>
2022-11-21 22:43:00 +0800

    add: 分页插件

---
+++ helpers/page.go
@@ -0,0 +1,108 @@
+package helpers
+
+const (
+	pageDefaultPageIndex = 1
+	pageDefaultPageSize  = 10
+)
+
+type (
+	Pager interface {
+		Offset() int
+		SetTotal(int)
+		Total() int
+		PageSize() int
+		NeedAll() bool
+	}
+	pager struct {
+		pageIndex int
+		pageSize  int
+		total     int
+		needAll   bool
+	}
+)
+
+func NewPager(pageIndex, pageSize int, needAll bool) Pager {
+	if pageIndex < 1 {
+		pageIndex = pageDefaultPageIndex
+	}
+	if pageSize <= 0 {
+		pageSize = pageDefaultPageSize
+	}
+	p := pager{
+		pageSize:  pageSize,
+		pageIndex: pageIndex,
+		needAll:   needAll,
+	}
+	return &p
+}
+
+func (p *pager) Offset() int {
+	return (p.pageIndex - 1) * p.pageSize
+}
+
+func (p *pager) SetTotal(total int) {
+	p.total = total
+}
+
+func (p *pager) Total() int {
+	return p.total
+}
+
+func (p *pager) PageSize() int {
+	return p.pageSize
+}
+
+func (p *pager) NeedAll() bool {
+	return p.needAll
+}
+
+type (
+	Int32Pager interface {
+		Offset() int32
+		SetTotal(int32)
+		Total() int32
+		PageSize() int32
+		NeedAll() bool
+	}
+	int32Pager struct {
+		pageIndex int32
+		pageSize  int32
+		total     int32
+		needAll   bool
+	}
+)
+
+func NewInt32Pager(pageIndex, pageSize int32, needAll bool) Int32Pager {
+	if pageIndex < 1 {
+		pageIndex = pageDefaultPageIndex
+	}
+	if pageSize <= 0 {
+		pageSize = pageDefaultPageSize
+	}
+	return &int32Pager{
+		pageIndex: pageIndex,
+		pageSize:  pageSize,
+		total:     0,
+		needAll:   needAll,
+	}
+}
+
+func (p *int32Pager) Offset() int32 {
+	return (p.pageIndex - 1) * p.pageSize
+}
+
+func (p *int32Pager) SetTotal(total int32) {
+	p.total = total
+}
+
+func (p *int32Pager) Total() int32 {
+	return p.total
+}
+
+func (p *int32Pager) PageSize() int32 {
+	return p.pageSize
+}
+
+func (p *int32Pager) NeedAll() bool {
+	return p.needAll
+}
